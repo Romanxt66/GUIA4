c3d603c5bbc2fcd1b186ab0d37f6ff8fdec25c47
Romanxt66 <nicolastorresrr@gmail.com>
2026-03-11 16:59:28 -0500

    Update models.py

--- models.py
+++ models.py
@@ -2,8 +2,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class Usuario(db.Model):
-    __tablename__ = 'usuarios' 
+    __tablename__ = 'usuarios'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
@@ -15,9 +17,20 @@ class Usuario(db.Model):
             "username": self.username,
             "rol": self.rol
         }
-    
-        
 
 
+class Producto(db.Model):
+    __tablename__ = 'productos'
 
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    cantidad = db.Column(db.Integer, nullable=False)
+    precio = db.Column(db.Float, nullable=False)
 
+    def serializar(self) -> dict:
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "cantidad": self.cantidad,
+            "precio": self.precio
+        }
